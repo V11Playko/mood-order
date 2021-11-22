f242677faa772f4e34a7562ac03ff6c2fd2b04a1
V11Playko <heinnervega20@gmail.com>
2021-11-22 07:49:01 -0500

    Se instalo nodemon y se esta probando el servidor

--- src/index.js
+++ src/index.js
@@ -2,11 +2,10 @@ const express = require('express');
 const app = express();
 const path = require('path');
 
+app.use(require('./routes/index'));
+
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.listen(3000, () => {
     console.log('Server on port 3000')
-})
-/*Prueba 1*/
-
-/* Prueba 2 */
+});
