01f434271ab5e926dbd6528bf79f5c0433b0df4d
V11Playko <heinnervega20@gmail.com>
2021-11-23 19:04:55 -0500

    Se encontro el error semantico que habia en el html y se corrigio el boton de envio

--- src/index.js
+++ src/index.js
@@ -2,10 +2,12 @@ const express = require('express');
 const app = express();
 const path = require('path');
 
+app.use(express.urlencoded({extended: false}));
+
 app.use(require('./routes/index'));
 
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.listen(3000, () => {
-    console.log('Server on port 3000')
+    console.log('Server on port 3000');
 });
