2ba7ad2a240d44fc484ff846eb17ae3f4f9c28c6
V11Playko <heinnervega20@gmail.com>
2021-12-02 07:02:57 -0500

    Se re-agrego archivo reouter

--- src/index.js
+++ src/index.js
@@ -17,18 +17,34 @@ app.post("/send-email", (req, res) => {
     const transporter = nodemailer.createTransport({
         host: 'smtp.ethereal.email',
         port: 587,
-        secure: false,
+        secure: false, // upgrade later with STARTTLS
         auth: {
             user: 'orlo.price5@ethereal.email',
             pass: 'fRN5Kqrr56ak6hcRXA'
-        }
+        },
+        tls: {
+            // do not fail on invalid certs
+            rejectUnauthorized: false,
+        },
     });
     var mailOptions = {
         from: "Mood Order",
         to:"heinnervega20@gmail.com",
         subject: 'Enviado desde nodemailer',
-        text: '!Hola, soy Mood Order!'
+        text: '!Hola, soy Mood Order!',
+        html: '<body>Hello World?</body>',
     }
+    // verify connection configuration
+    transporter.verify(function (error, success) {
+        if (error) {
+            console.log(error);
+        } else {
+            console.log("Server is ready to take our messages");
+        }
+    });
+    /* However, note that this call only tests connection and authentication, but does not check whether the service allows you to use a specific sender address or not. */
+
+    
     transporter.sendMail(mailOptions, (error,info) => {
         if (error) {
             res.status(500).send(error.message);
